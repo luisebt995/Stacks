150f00695d99175c6a7797590f89c3ff2afd06d1
Luis Bustamante <luisebt995@gmail.com>
2024-04-04 18:13:02 +0200

    Implementation of H, V and ZStack.

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Header()
+            HStack{
+                Fighter(name: "Goku", power: 800, description: "Ultra Instinct", colorText: .white, colorBackground: .mint)
+                ZStack {
+                    Fighter(name: "Vegeta", power: 700, description: "SSJ3", colorText: .black, colorBackground: Color(red: 237/255, green: 64/255, blue: 90/255))
+                    Text("Todo lo puedo en cristo que me fortalece")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .background(.yellow)
+                        .offset(x: 0 , y: 50)
+                }
+            }
+            .padding(.horizontal, 5)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 5){
+            Text("Choose")
+                .font(.system(.largeTitle, design: .rounded))
+                .foregroundColor(.purple)
+                .fontWeight(.black)
+            Text("your")
+                .font(.system(.largeTitle, design: .rounded))
+                .foregroundColor(.purple)
+                .fontWeight(.heavy)
+            Text("Fighter")
+                .font(.system(.largeTitle, design: .rounded))
+                .foregroundColor(.purple)
+                .fontWeight(.bold)
+        }
+    }
+}
+
+struct Fighter: View {
+    let name : String
+    let power : Int
+    let description : String
+    
+    let colorText : Color
+    let colorBackground : Color
+    var body: some View {
+        VStack{
+            Text("\(name)")
+                .font(.system(.title, design: .rounded))
+                .foregroundColor(colorText)
+                .fontWeight(.black)
+            Text("\(power)/1000")
+                .font(.system(size: 12, weight: .medium, design: .rounded))
+                .foregroundColor(colorText)
+            Text("Ultra Instinct")
+                .font(.system(size: 12, weight: .medium, design: .rounded))
+                .foregroundColor(colorText)
+        }
+        //.frame(width: 100, height: 100)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 50, maxHeight: 100)
+        .padding(30)
+        .background(colorBackground)
+        .cornerRadius(20)
     }
 }
