bfbce882af391f8d027302c114b1afa95761b971
Luis Bustamante <luisebt995@gmail.com>
2024-04-04 15:30:20 +0200

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Luis on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
